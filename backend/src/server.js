77dc7cd87963b2c31df824298553f32be6ddd6af
sebspetersen <sebspetersen@gmail.com>
2024-07-16 11:42:37 +0200

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Alteryx Workflow Scheduler API');
+});
+
+// TODO: Add routes for scheduling workflows
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
