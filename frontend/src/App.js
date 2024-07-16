6fb54319d83fc356a587f66f9c7665760ee5495e
sebspetersen <sebspetersen@gmail.com>
2024-07-16 11:42:13 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import WorkflowScheduler from './components/WorkflowScheduler';
 
+function App() {
+  return (
+    <div className="App">
+      <WorkflowScheduler />
+    </div>
+  );
+}
+
+export default App;
